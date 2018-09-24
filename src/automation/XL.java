8e8b67763fa728dbe3e68f32e548f6e4f9526977
sindhu <sindhushashi93@gmail.com>
2018-09-24 15:28:35 +0530

    Initial commit

---
+++ src/automation/XL.java
@@ -0,0 +1,20 @@
+package automation;
+
+//import java.io.File;
+//import java.io.IOException;
+//
+//import org.apache.poi.EncryptedDocumentException;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class XL {
+	
+	public static void main(String[] args) throws Exception
+	{
+//		Workbook w = WorkbookFactory.create(new File("./Book1.xlsx"));
+//		String s = w.getSheet("sheet1").getRow(0).getCell(0).toString();
+//		System.out.println(s);
+		
+	}
+
+}
